5ff4c348269a896f55bdcc1fe0c2e19284fd5cab
Alexandr Tichon <tsikhan.p@gmail.com>
2019-05-23 17:10:26 +0300

    fix: stop and spin actions

--- src/api/wheel.ts
+++ src/api/wheel.ts
@@ -31,7 +31,7 @@ export function wheelCanStop(position: number, speed: number) {
   const { wheelInterval, symbolSizes } = SPRITE_SIZES;
 
   for (let i = 0; i < Math.abs(speed); i++) {
-    if ((Math.abs(position) + wheelInterval + i) % symbolSizes.height === 0) {
+    if ((Math.abs(position) - wheelInterval + i) % symbolSizes.height === 0) {
       let stopDistance = i;
       if (speed < 0) {
         stopDistance = -i;
@@ -51,3 +51,19 @@ export function wheelSpeedShouldReverse(position: number, height: number) {
   const { backgroundSizes } = SPRITE_SIZES;
   return (Math.abs(position) - height === 0);
 }
+
+/*
+    wheel.position.set(
+      (178 + wheelInterval) * i - wheelInterval,
+      -wheel.height + backgroundSizes.height,
+    );
+
+        if ( wheel.y + wheelSpeed[index] <= 0 &&
+      Math.abs(wheel.y) <= symbolSizes.height
+    ) {
+      const additionalWheel = createWheel(13, "../assets/img/symbols/");
+      // console.log(wheel.y);
+      additionalWheel.position.set(0, -wheel.height);
+      wheel.addChild(additionalWheel);
+    }
+ */
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import * as PIXI from "pixi.js";
 
 import { drawGraphics } from "./api/graphics";
-import { createWheel, wheelCanStop, wheelSpeedShouldReverse } from "./api/wheel";
-import { APP_BACKGROUND, APP_SCREEN_SIZES, SPIN_BTN } from "./config/app-config";
 import {
-  APP_SOUNDS,  Application,  Container,
-  images,  initialWheelSpeed,  loader,
-  resources,  Sprite,
+  createWheel,
+  wheelCanStop,
+  wheelSpeedShouldReverse,
+} from "./api/wheel";
+import {
+  APP_BACKGROUND,
+  APP_SCREEN_SIZES,
+  SPIN_BTN,
+} from "./config/app-config";
+import {
+  APP_SOUNDS,
+  Application,
+  Container,
+  images,
+  initialWheelSpeed,
+  loader,
+  resources,
+  Sprite,
   SPRITE_SIZES,
 } from "./config/data-config";
 import "./index.scss";
@@ -29,20 +42,30 @@ const homeScene = new Container();
 const wheelContainer: object[] = [];
 let wheelSpeed: number[] = [];
 let previousWheelSpeed: number[] = [];
+const wheelNumArr: number[] = [0, 0, 0, 0, 0];
+const delayEndArr: number[] = [0, 0, 0, 0, 0];
+let stoppedWheels = 0;
 
-loader
-  .add(images)
-  .load(setup);
+loader.add(images).load(setup);
 
 function setup() {
-  const {wheelInterval, backgroundElementSizes, symbolSizes} = SPRITE_SIZES;
+  const {
+    wheelInterval,
+    backgroundElementSizes,
+    symbolSizes,
+    backgroundSizes,
+  } = SPRITE_SIZES;
 
   // draw background
-  const slotOverlay = new Sprite(resources["../assets/img/slotOverlay.png"].texture);
+  const slotOverlay = new Sprite(
+    resources["../assets/img/slotOverlay.png"].texture,
+  );
   for (let j = 0; j < 6; j++) {
     const slotColumn = new Container();
     for (let i = 0; i < 5; i++) {
-      const slotBackground = new Sprite(resources["../assets/img/winningFrameBackground.jpg"].texture);
+      const slotBackground = new Sprite(
+        resources["../assets/img/winningFrameBackground.jpg"].texture,
+      );
       slotBackground.position.set(0, backgroundElementSizes.height * i);
       slotColumn.addChild(slotBackground);
     }
@@ -53,7 +76,10 @@ function setup() {
   // gaming wheels (5 instance)
   for (let i = 0; i < 5; i++) {
     const wheel = createWheel(13, "../assets/img/symbols/");
-    wheel.position.set(((178 + wheelInterval) * i - wheelInterval), wheelInterval);
+    wheel.position.set(
+      (178 + wheelInterval) * i - wheelInterval,
+      -wheel.height + backgroundSizes.height,
+    );
     wheelContainer.push(wheel);
     homeScene.addChild(wheel);
   }
@@ -69,11 +95,12 @@ function setup() {
   // add spin-btn handler
   SPIN_BTN.addEventListener("click", () => {
     if (state !== spin) {
-      APP_SOUNDS.reelSpin.play();  // play sounds
+      APP_SOUNDS.reelSpin.play(); // play sounds
       SPIN_BTN.setAttribute("disabled", "");
       if (state) {
         state = spin;
         wheelSpeed = [...previousWheelSpeed];
+        stoppedWheels = 0;
       } else {
         state = spin;
         wheelSpeed = [...initialWheelSpeed];
@@ -96,29 +123,42 @@ function gameLoop() {
 
 function spin() {
   // Configure speed of wheels
-  const { backgroundSizes, stdWheelHeight, symbolSizes } = SPRITE_SIZES;
+  const { symbolSizes } = SPRITE_SIZES;
   wheelContainer.forEach((wheel: PIXI.Sprite, index) => {
-    if (wheel.y + wheel.height <= backgroundSizes.height + symbolSizes.height) {
+    if (Math.abs(wheel.y - 2314 * wheelNumArr[index]) <= symbolSizes.height) {
       const additionalWheel = createWheel(13, "../assets/img/symbols/");
-      additionalWheel.position.set(0, wheel.height);
+      additionalWheel.position.set(0, -2314 * (wheelNumArr[index] + 1));
       wheel.addChild(additionalWheel);
+      wheelNumArr[index] += 1;
     }
     wheel.y += wheelSpeed[index];
   });
 }
 
 function stop() {
+  const { symbolSizes } = SPRITE_SIZES;
   wheelContainer.forEach((wheel: PIXI.Sprite, index) => {
     const stopData = wheelCanStop(wheel.y, wheelSpeed[index]);
-    if (stopData.flag) {
+    if (Math.abs(wheel.y - 2314 * wheelNumArr[index]) <= symbolSizes.height) {
+      const additionalWheel = createWheel(13, "../assets/img/symbols/");
+      additionalWheel.position.set(0, -2314 * (wheelNumArr[index] + 1));
+      wheel.addChild(additionalWheel);
+      wheelNumArr[index] += 1;
+    }
+    let delayFlag = true;
+    if (index > 0) {
+      const currentTime = new Date();
+      const delay = currentTime.getTime() - delayEndArr[index - 1];
+      delayFlag = delay > 200;
+    }
+    if (stopData.flag && index === stoppedWheels && delayFlag) {
       wheel.y += stopData.stopDistance;
       wheelSpeed[index] = 0;
+      const time = new Date();
+      delayEndArr[index] = time.getTime();
+      stoppedWheels += 1;
       return true;
     } else {
-      if (wheelSpeedShouldReverse(wheel.y, wheel.height)) {
-        wheelSpeed[index] = - wheelSpeed[index];
-        previousWheelSpeed[index] = - wheelSpeed[index];
-      }
       wheel.y += wheelSpeed[index];
     }
   });
